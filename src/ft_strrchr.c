1c72cafc8b944474e5a665e5d161bc8a168ed1e3
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-01-15 17:01:46 +0100

    ft_strrchr.c

---
+++ src/ft_strrchr.c
@@ -0,0 +1,64 @@
+/*
+SYNOPSIS
+     #include <string.h>
+
+     char *
+     strchr(const char *s, int c);
+
+     char *
+     strrchr(const char *s, int c);
+
+DESCRIPTION
+     The strchr() function locates the first occurrence of c (converted to a
+     char) in the string pointed to by s.  The terminating null character is
+     considered to be part of the string; therefore if c is ‘\0’, the functions
+     locate the terminating ‘\0’.
+
+     The strrchr() function is identical to strchr(), except it locates the
+     last occurrence of c.
+
+RETURN VALUES
+     The functions strchr() and strrchr() return a pointer to the located
+     character, or NULL if the character does not appear in the string.
+*/
+# include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+	unsigned char	d;
+
+	d = (unsigned char)c;
+	i = 0;
+	while (s[i])
+		i++;
+	while (i != 0)
+	{
+		if ((unsigned char)s[i] == d)
+			 return ((char *)s + i);
+		i--;
+	}
+	if ((unsigned char)s[0] == d)
+		return ((char *)s);
+	return (NULL);
+}
+/*
+#include <string.h>
+
+int	main(void)
+{
+	//int	c = 'o';
+	//int	c = '\0';
+	int	c = 'a';
+	//const char	s1[] = "Hello 42";
+	//const char	s1[] = "";
+	const char	s1[] = "abca";
+	char	*a = ft_strrchr(s1, c);
+	char	*b = strrchr(s1, c);
+
+	printf("El caracter %d está en %s en esta dirección:\n", c, s1);
+	printf("Mine: %p\n", a);
+	printf("Theirs: %p\n", b);
+	return (0);
+}
+*/
---
+++ src/libft.h
@@ -0,0 +1,27 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Part 1
+int		ft_isalpha(int c);
+int		ft_isdigit(int d);
+int		ft_isalnum(int a);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+
+#endif
