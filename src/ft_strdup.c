d75ec7fcc128b34702c5dad898a6a45433ded45f
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-01-18 16:40:23 +0100

    ft_strdup.c

---
+++ src/ft_strdup.c
@@ -0,0 +1,61 @@
+/*
+SYNOPSIS
+     #include <string.h>
+
+     char *strdup(const char *s1);
+
+DESCRIPTION
+     The strdup() function allocates sufficient memory for a copy of the
+     string s1, does the copy, and returns a pointer to it.  The pointer
+     may subsequently be used as an argument to the function free(3).
+
+     If insufficient memory is available, NULL is returned and errno is set
+     to ENOMEM.
+>> Como es una STR cada caracter es un byte, entonces count == size. 
+No hace falta comprobar (count > SIZE_MAX / size).
+En  malloc(count * size), como size es 1 => malloc(count)
+Le sumo 1 a len para tener espacio para el '\0'
+*/
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	count;
+	size_t	len;
+	char	*ptr;
+	size_t	i;
+
+	len =  ft_strlen(s1);
+	count = len + 1;
+	ptr = malloc(count);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+/*
+#include <string.h>
+
+int	main(void)
+{
+	const char	s1[] = "Hello 42";
+	//const char	s1[] = "";
+	char	*s2;
+	char	*s3;
+
+	s2 = ft_strdup(s1);
+	s3 = strdup(s1);
+	printf("Mine is: %s\n", s2);
+	printf("Theirs is: %s\n", s3);
+	free(s2);
+	free(s3);
+	return (0);
+}
+*/
--- src/libft.h
+++ src/libft.h
@@ -31,5 +31,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
