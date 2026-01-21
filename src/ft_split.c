a2e9ac7f5336154243280d7acebbfd4bdbff9e7a
Javier Sánchez de Arévalo López <Javier2@iMac-de-Javier.local>
2026-01-21 15:16:04 +0100

    ft_split.c

--- src/ft_split.c
+++ src/ft_split.c
@@ -19,8 +19,97 @@ utilizando el carácter ‘c’ como delimitador.
 El arreglo debe terminar con un puntero a NULL.
 */
 
+#include "libft.h"
+
+static size_t	x_slices(char const *s, char c)
+{
+	size_t	slices;
+	size_t	i;
+
+	slices = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		if (s[i])
+		{
+			slices++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (slices);
+}
+
+static char	**free_all(char **tab, size_t blocks)
+{
+	while (blocks > 0)
+	{
+		blocks--;
+		free(tab[blocks]);
+	}
+	free(tab);
+	return (NULL);
+}
+
+static char	**placing(const char *s, char c, char **tab, size_t slices)
+{
+	size_t	i;
+	size_t	ini;
+	size_t	blocks;
+
+	i = 0;
+	blocks = 0;
+	while (blocks < slices)
+	{
+		while (s[i] && s[i] == c)
+			i++;
+		ini = i;
+		while (s[i] && s[i] != c)
+			i++;
+		tab[blocks] = ft_substr(s, ini, i - ini);
+		if (!tab[blocks])
+			return (free_all(tab, blocks));
+		blocks++;
+	}
+	tab[blocks] = NULL;
+	return (tab);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	char	**tab;
+	size_t	slices;
 
-
+	if (!s)
+		return (NULL);
+	slices = x_slices(s, c);
+	tab = malloc((slices + 1) * sizeof(char *));
+	if (!tab)
+		return (NULL);
+	tab = placing(s, c, tab, slices);
+	if (!tab)
+		return (NULL);
+	return (tab);
 }
+/*
+int	main(void)
+{
+	const char	s[] = "  La Rosa de los vientos";
+	//char	c = 'o';
+	char	c = ' ';
+	size_t	i = 0;
+	char	**tab;
+
+	tab = ft_split(s, c);
+	while (tab[i])
+	{
+		printf("%s\n", tab[i]);
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (0);
+}
+*/
